5be71cc5d3b2162ba67b22e151a8a5bb4d766a8e
jangid-ku-jitendra <168421373+jangid-ku-jitendra@users.noreply.github.com>
2024-05-11 12:22:41 +0530

    main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { NotesDetailsComponent } from './pages/notes-details/notes-details.component';
+import { MainComponent } from './pages/main/main.component';
 
 export const routes: Routes = [
     {
         path: "note/:id",
         component: NotesDetailsComponent
+    },
+    {
+        path: "",
+        component: MainComponent
     }
 ];
