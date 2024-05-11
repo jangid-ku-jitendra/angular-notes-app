a03610ddc22a6a21d6a8e04c4deb9ee20d650284
jangid-ku-jitendra <168421373+jangid-ku-jitendra@users.noreply.github.com>
2024-05-12 01:18:17 +0530

    more

--- src/app/pages/notes-details/notes-details.component.ts
+++ src/app/pages/notes-details/notes-details.component.ts
@@ -1,20 +1,59 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import $ from 'jquery';
 
 @Component({
-  selector: 'app-notes-details',
-  standalone: true,
-  imports: [],
-  templateUrl: './notes-details.component.html',
-  styleUrl: './notes-details.component.scss'
+	selector: 'app-notes-details',
+	standalone: true,
+	imports: [CommonModule, FormsModule],
+	templateUrl: './notes-details.component.html',
+	styleUrl: './notes-details.component.scss'
 })
 export class NotesDetailsComponent {
-  actualInnerWidth = document.body.clientWidth;     // El. width minus scrollbar width
-  actualInnerWidth2 = document.body.scrollWidth;
+	actualInnerWidth = document.body.clientWidth;     // El. width minus scrollbar width
+	actualInnerWidth2 = document.body.scrollWidth;
+	selectedSubtext: string | undefined;
+	textData: any
 
-  constructor() {
+	constructor() {
+		$(".container-full").width(this.actualInnerWidth)
+	}
 
-    console.log(`actualInnerWidth ${this.actualInnerWidth} actualInnerWidth2 ${this.actualInnerWidth2}`)
-    $(".container-full").width(this.actualInnerWidth)
-  }
+	replaceSelectedText(styleType: string): void {
+		const selection = window.getSelection();
+		if (selection && selection.rangeCount > 0) {
+			const range = selection.getRangeAt(0);
+			const selectedText = selection.toString();
+			let replaceText = null;
+			switch(styleType) {
+				case "make-bold": {
+					replaceText = `<span style="font-weight: bold">${selectedText}</span>`; // Replace with your desired replacement text
+				}
+				break;
+				case "remove-bold": {
+
+				}
+				break;
+				case "make-italic": {
+					replaceText = `<span style="font-style: italic;">${selectedText}</span>`; // Replace with your desired replacement text
+				}
+			}
+
+			if(replaceText) {
+				const fragment = range.createContextualFragment(replaceText);
+				range.deleteContents();
+				range.insertNode(fragment);
+			}
+		}
+	}
+
+	makeBold() {
+		this.replaceSelectedText("make-bold")
+	}
+
+	makeItalic() {
+		this.replaceSelectedText("make-italic")
+	}
+
 }
